296db2cd298ed8ac0053ef3ebc9f940b22b08ec1
ShiYiZhiHanZhuXhuYa <3552354372@qq.com>
2023-08-21 15:56:20 +0800

    修复

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -42,6 +42,7 @@ export default defineUserConfig({
     style: "@vuepress-reco/style-default",
     logo: "https://python-office-1300615378.cos.ap-chongqing.myqcloud.com/icon2.jpg",
     author: "晚枫",
+    base: "python-office-new",
     authorAvatar: "/head.png",
     docsRepo: "https://gitee.com/CoderWanFeng/python-office.com",
     docsBranch: "main",
